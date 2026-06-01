972be992296fa97ec819e63235d07ed68cbb46a9
LukBomfim <lucasbomfim2801@gmail.com>
2026-05-31 21:32:59 -0300

    criação do app.js

---
+++ backend/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT || 3000}`)
+})
